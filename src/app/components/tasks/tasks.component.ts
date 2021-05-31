191e65e44f1eab92e9762655ed87a1c2132fb8e3
Md. Ehtisham Farooqui <mefxe01@gmail.com>
2021-05-31 22:23:04 +0530

    added task component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
+import { TaskService } from './../../services/task.service';
 import { Task } from './../../Task';
-import { TASKS } from './../../mock-tasks';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.tasks = this.taskService.getTasks()
   }
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,17 @@
+import { TASKS } from './../mock-tasks';
+import { Task } from './../Task';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
+
   constructor() { }
+
+  getTasks(): Task[] {
+    return TASKS;
+  }
 }
